436ee6a4ea3450e6c3d3b679d9759f0a91d86cbc
Amani <alouendr@uottawa.ca>
2024-11-23 09:23:26 -0500

    added comments to deck

--- Deck.cpp
+++ Deck.cpp
@@ -3,12 +3,12 @@
 
 
 
-//Deck::Deck(std::istream& input, const CardFactory* factory) {
-//	std::cout << "constructeur avec deux args";
-//}
-
 class CardFactory;
 
+/// <summary>
+/// Copy constructor for the deck
+/// </summary>
+/// <param name="d">Deck to copy into the new deck</param>
 Deck::Deck(const Deck& d) {
 	std::cout << "Deck copy constructor for deck\n";
 	for (Card* card : d) {
@@ -16,13 +16,20 @@ Deck::Deck(const Deck& d) {
 	}
 }
 
+
+/// <summary>
+/// A constructor with two parameters that is responsible to construct a deck from a given input file, used when reloading the game from a file
+/// </summary>
+/// <param name="input">Input file with all the cards e.g: R B C S g</param>
+/// <param name="factory">Object responsible of creating, deleting and keeping track of cards, according to the factory pattern</param>
 Deck::Deck(std::istream& input, const CardFactory* factory) {
-	long size = input.tellg();
+	long size = input.tellg();// assuming the file pointer is at the end of file, using std::ios::ate
 	//std::cout << "The size is: " << size << "\n";
 	input.seekg(0, std::ios::beg);
 	char* tab = new char[size];
 	input.read(tab, size);
 	int i{ 0 };
+	// depending on the card symbol get the right card
 	while (i < size) {
 		if (tab[i] != ' ') {
 			//std::cout << tab[i];
@@ -58,16 +65,32 @@ Deck::Deck(std::istream& input, const CardFactory* factory) {
 	delete[] tab;
 }
 
+
+/// <summary>
+/// Deck destructor
+/// </summary>
 Deck::~Deck() {
 	std::cout << "\ndestructing my deck\n";
 }
 
+
+/// <summary>
+/// This method deletes the last card on the deck container
+/// </summary>
+/// <returns>Card deleted</returns>
 Card* Deck::draw() {
 	Card* cardUp{ back() };
 	pop_back();
 	return cardUp;
 }
 
+
+/// <summary>
+/// This method prints all the cards present in the deck on the output stream
+/// </summary>
+/// <param name="output">File or stream on which to print the content of the deck</param>
+/// <param name="deck">The actual deck to print</param>
+/// <returns>The stream where the writing took place</returns>
 std::ostream& operator << (std::ostream& output, Deck deck) {
 	//output << "Cards present in the deck: \n";
 	int i{ 0 };
--- Deck.h
+++ Deck.h
@@ -10,13 +10,47 @@ class CardFactory;
 class Deck : public std::vector<Card*>{
 	// why would it derive from std::vector??
 public:
+	/// <summary>
+	/// Default deck constructor
+	/// </summary>
 	Deck() {
-		std::cout << "default constructor for deck\n";
+		//std::cout << "default constructor for deck\n";
 	}
 
+
+	/// <summary>
+	/// Copy constructor for the deck
+	/// </summary>
+	/// <param name="d">Deck to copy into the new deck</param>
 	Deck(const Deck& d);
+
+
+	/// <summary>
+	/// A constructor with two parameters that is responsible to construct a deck from a given input file, used when reloading the game from a file
+	/// </summary>
+	/// <param name="input">Input file with all the cards e.g: R B C S g</param>
+	/// <param name="factory">Object responsible of creating, deleting and keeping track of cards, according to the factory pattern</param>
 	Deck(std::istream& input, const CardFactory* factory);
+
+
+	/// <summary>
+	/// Deck destructor
+	/// </summary>
 	~Deck();
+
+
+	/// <summary>
+	/// This method deletes the last card on the deck container
+	/// </summary>
+	/// <returns>Card deleted</returns>
 	Card* draw();
+
+
+	/// <summary>
+	/// This method prints all the cards present in the deck on the output stream
+	/// </summary>
+	/// <param name="output">File or stream on which to print the content of the deck</param>
+	/// <param name="deck">The actual deck to print</param>
+	/// <returns>The stream where the writing took place</returns>
 	friend std::ostream& operator << (std::ostream& output, Deck deck);
 };
